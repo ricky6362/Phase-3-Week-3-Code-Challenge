985e10b65df1378b6ff5a5f94272424d1e2e62cc
ricky6362 <63843083+ricky6362@users.noreply.github.com>
2023-09-07 23:04:42 +0300

    Minor Changes to the main file

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models.customer import Customer
 
-# Rest of your code
-
 
 # Initialize your SQLAlchemy engine and session
 engine = create_engine('postgresql://postgres:password@localhost:5432/restaurant_db')
